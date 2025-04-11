4be6e507c294fc943d61f489e00139fe39b86e6b
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-04-12 00:59:14 +0100

    Add initial set of files from Hackerrank

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+package com.andrealaforgia
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'activityNotifications' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY expenditure
+ *  2. INTEGER d
+ */
+
+fun activityNotifications(expenditure: Array<Int>, d: Int): Int {
+    // Write your code here
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val d = first_multiple_input[1].toInt()
+
+    val expenditure = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = activityNotifications(expenditure, d)
+
+    println(result)
+}
