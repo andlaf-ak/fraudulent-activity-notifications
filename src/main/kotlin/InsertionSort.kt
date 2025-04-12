93d11bff47919bec8c3c1db8aed23246628ef689
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-04-12 19:15:33 +0100

    Green phase: add trivial implementation

--- src/main/kotlin/InsertionSort.kt
+++ src/main/kotlin/InsertionSort.kt
@@ -1,5 +1,8 @@
 package com.andrealaforgia
 
-fun insertInSortedArray(values: Array<Int>, value: Int) {
-    return
+fun insertInSortedArray(values: MutableList<Int>, value: Int) {
+    values.add(values[4])
+    values[4] = values[3]
+    values[3] = values[2]
+    values[2] = 3
 }
--- src/test/kotlin/InsertionSortKtTest.kt
+++ src/test/kotlin/InsertionSortKtTest.kt
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test
 class InsertionSortKtTest {
     @Test
     fun shouldInsertInSortedArray() {
-        val values = arrayOf(1, 2, 4, 5, 6)
-        assertEquals(arrayOf(1, 2, 3, 4, 5, 6), insertInSortedArray(values, 3))
+        val values = mutableListOf(1, 2, 4, 5, 6)
+        insertInSortedArray(values, 3)
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), values)
     }
 }
