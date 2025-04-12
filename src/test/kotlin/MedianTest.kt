52a69e2691e0f96941ec136730ca396203f1263a
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-04-12 02:23:13 +0100

    Red phase: add failing test for median on array with 1 element

--- src/test/kotlin/MedianTest.kt
+++ src/test/kotlin/MedianTest.kt
@@ -8,4 +8,10 @@ class MedianTest {
         val values = emptyArray<Int>()
         assertEquals(0, median(values))
     }
+
+    @Test
+    fun shouldCalculateMedianForArrayWithOneElement() {
+        val values = arrayOf(10)
+        assertEquals(10, median(values))
+    }
 }
